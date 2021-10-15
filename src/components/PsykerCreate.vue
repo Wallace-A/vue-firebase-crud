4ba34eca6ff819b1a11c50ce6068c884f6a80ab3
Wallace-A <andrew.wallace.english@gmail.com>
2021-10-15 13:35:11 +0900

    type dropdown, power list on create and edit page

--- src/components/PsykerCreate.vue
+++ src/components/PsykerCreate.vue
@@ -8,14 +8,23 @@
 
             <div class="form-group mt-3">
                 <label>Type</label>
-                <input 
-                v-model="form.type"
-                class="form-control"
-                type="text"
-                required
-                >
+                    <select v-model="form.type" @change="calculateStats" class="form-control" required>
+                        <option disabled value="">Please select one</option>
+                        <option>Thousand Sons Daemon Prince</option>
+                        <option>Exalted Sorcerer</option>
+                        <option>Sorcerer</option>
+                        <option>Sorcerer in Terminator Armour</option>
+                        <option>Infernal Master</option>
+                        <option>Tzaangor Shaman</option>
+                    </select>
             </div>
-
+            <div class="form-group mt-3">
+                <label>Powers</label>
+                <div v-for="(power) in powers" :key="power.id" @change="checkPowers">
+                    <label :for="power.name">{{power.name}}</label>
+                    <input type="checkbox" id="power" :value="power.name" v-model="form.powers">
+                </div>
+            </div>
             <button type="submit" class="btn btn-success mt-3">
                 Create Psyker
             </button>
@@ -27,21 +36,41 @@
 <script>
 import { createPsyker } from "@/firebase";
 import { reactive } from "vue";
+import unitList from "@/unitStats.json";
+import powerList from "@/powers.json";
 
 export default {
     setup() {
         const form = reactive({
             name: "",
-            type: ""
+            type: "",
+            powers: []
         })
-
+        const units = unitList;
+        const powers = powerList;
         const onSubmit = async () => {
             await createPsyker({...form})
             //reset form inputs
             form.name = "";
             form.type = "";
+            form.powers = [];
         }
-        return { form, onSubmit }
+
+        const calculateStats = () => {
+            console.log("calculate stats");
+            const stats = units;
+            console.log(stats);
+        }
+
+        const checkPowers = () => {
+            console.log(form.powers);
+        }
+        return { form, units, powers, onSubmit, calculateStats, checkPowers }
+    },
+    data() {
+        return {
+
+        }
     }
     
 }
